226c82c6f1f127683e77bcd83e40480ecd8ca63b
Brandon Bakus <brandonbakus@gmail.com>
2025-02-20 13:31:36 -0500

    tough one

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, word_list):
+        word_sorted = sorted(self.word.lower())
+        matches = []
+        
+        for item in word_list:
+            if item.lower() != self.word.lower():  
+                if sorted(item.lower()) == word_sorted:
+                    matches.append(item)
+                    
+        return matches
+
+
